3fc74d53e6d5cb7b1cb29102851acd498822b124
LegendaryFire <TristanBalon@Gmail.com>
2022-06-20 23:34:54 -0600

    Created vehicle model in inventory app. Created the start of the inventory overview. Added URL routing for the inventory app. Made migrations for the inventory app.

--- balonPos/urls.py
+++ balonPos/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from users.views import login_view, logout_view
 
 urlpatterns = [
     path('', login_view, name='login'),
+    path('inventory/', include('inventory.urls')),
     path('admin/', admin.site.urls),
 ]
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    KILOMETERS = 'km'
+    MILES = 'mi'
+    mileage_choices = [
+        (KILOMETERS, 'Kilometers'),
+        (MILES, 'Miles')
+    ]
+
+    USED = 'used'
+    NEW = 'new'
+    condition_choices = [
+        (USED, 'Used'),
+        (NEW, 'New')
+    ]
+
+    GAS = 'gas'
+    DIESEL = 'diesel'
+    ELECTRIC = 'electric'
+    engine_choices = [
+        (GAS, 'Gas'),
+        (DIESEL, 'Diesel'),
+        (ELECTRIC, 'Electric')
+    ]
+
+    stock = models.BigAutoField(primary_key=True, verbose_name="Stock")
+    branch = models.ForeignKey('users.Branch', on_delete=models.RESTRICT, verbose_name="Branch")
+
+    vin = models.CharField(max_length=17, verbose_name='VIN')
+    year = models.IntegerField(verbose_name='Year')
+    make = models.CharField(max_length=30, verbose_name='Make')
+    model = models.CharField(max_length=30, verbose_name='Model')
+    trim = models.CharField(max_length=30, verbose_name='Trim')
+    mileage = models.IntegerField(verbose_name='Mileage')
+    mileage_units = models.CharField(choices=mileage_choices, default=KILOMETERS, max_length=2, verbose_name='Mileage Units')
+
+    condition = models.CharField(max_length=4, choices=condition_choices, default=USED, verbose_name="Type")
+    color = models.CharField(max_length=30, verbose_name='Color')
+    body_style = models.CharField(max_length=20, null=True, blank=True, verbose_name='Body Style')
+    body_type = models.CharField(max_length=20, null=True, blank=True, verbose_name='Body Type')
+    engine_type = models.CharField(choices=engine_choices, default=GAS, max_length=8, verbose_name='Engine Type')
+
+    def __str__(self):
+        return '%d %s %s %s - %s' % (self.year, self.make, self.model, self.trim, self.vin[-6:])

13496eecb91264c27b06b95bf591763f696ad8e7
LegendaryFire <TristanBalon@Gmail.com>
2022-06-25 13:55:33 -0600

    Added URL routing for adding, editing and selling a vehicle. Updated frontend to consider the active page. Added URL routes for adding a vehicle in the front end.

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,5 +5,7 @@ app_name = 'inventory'
 urlpatterns = [
     path('tables/inventory', tables.InventoryTableView.as_view(), name="inventory_table"),
     path('', views.view_inventory, name='overview'),
-    path('vehicle/', views.view_vehicle, name='vehicle'),
+    path('vehicle/add/', views.view_vehicle, name='add_vehicle'),
+    path('vehicle/edit/<int:stock>/', views.view_vehicle, name='edit_vehicle'),
+    path('vehicle/sell/<int:stock>/', views.view_vehicle, name='sell_vehicle'),
 ]

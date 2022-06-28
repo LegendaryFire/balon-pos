825226f3a113608bfa719c9307f869ab502111bb
LegendaryFire <TristanBalon@Gmail.com>
2022-06-27 18:36:51 -0600

    Created registration model. Made new migrations.

--- registrations/models.py
+++ registrations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Registration(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    charged = models.DecimalField(max_digits=6, default=0.00, decimal_places=2, verbose_name="Charged")
+    date_requested = models.DateField(default=None, null=True, blank=True, verbose_name="Date Requested")
+    date_received = models.DateField(default=None, null=True, blank=True, verbose_name="Date Received")
+    scan = models.ImageField(null=True, blank=True, verbose_name="Registration", upload_to="registrations/")
+    sales_order = models.OneToOneField('sales.SalesOrder', on_delete=models.CASCADE, verbose_name="Sales Order")
